cdb91336883c155f977526bdc4527f592a228da8
waleedshkt <waleedshkt@gmail.com>
2025-03-14 01:06:44 +0500

    Completed subscribed modal

--- src/components/Modals/Subscribed/index.js
+++ src/components/Modals/Subscribed/index.js
@@ -1,12 +1,47 @@
-import * as React from "react";
-import "antd/dist/antd.min.css";
-import { Button } from "antd";
-import PropTypes from "prop-types";
-import * as styles from "./modal-subscribe3.module.css";
+import React, { memo, useCallback, useContext } from "react";
+import { useHookstate as useState } from "@hookstate/core";
+import { Modal } from "antd";
+import * as styles from "./index.module.css";
 
-const ModalSubscribe3 = ({ className = "", onClose }) => {
+import { isAuth, misc, subscription } from "../../../globalStates";
+import { AuthContext } from "../../../helpers/auth/firebase/user";
+
+import { getFormattedDate, getFutureFormattedDate } from "../../../helpers/date";
+
+const SubscribedModal = ({ open, onClose }) => {
+  const { signOut } = useContext(AuthContext);
+
+  const isAuth_ = useState(isAuth); //global
+  const misc_ = useState(misc); //global
+  const { lastInvoiceDate, cardEnding } = useState(subscription); //global
+
+  const handleSignOut = useCallback(async () => {
+    try {
+      misc_.isLoading.set(true);
+      
+      await signOut();
+
+      isAuth_.set(false);
+      misc_.isLoading?.set(false);
+    }catch(e) {
+      console.error(e);
+
+      misc_.set({
+        isLoading: false,
+        message: { type: "error", content: "Something went wrong. Please try again" }
+      });
+    }
+  }, [signOut]);
+
   return (
-    <div className={[styles.modalSubscribe4, className].join(" ")}>
+    <Modal 
+      open={open}
+      okText="Close"
+      cancelText="Logout"
+      onCancel={handleSignOut}
+      onOk={onClose}
+      className={styles.modalSubscribe4}
+    >
       <div className={styles.content}>
         <div className={styles.welcomePremiumSubscriber}>
           Welcome, Premium Subscriber!
@@ -18,17 +53,11 @@ const ModalSubscribe3 = ({ className = "", onClose }) => {
                 <div className={styles.label}>Subscription</div>
                 <div className={styles.text}>Yearly</div>
               </div>
-              <div className={styles.hintText}>
-                This is a hint text to help user.
-              </div>
             </div>
             <div className={styles.inputFieldBase}>
               <div className={styles.inputWithLabel}>
                 <div className={styles.label}>Expires on</div>
-                <div className={styles.text}>27 February 2026</div>
-              </div>
-              <div className={styles.hintText}>
-                This is a hint text to help user.
+                <div className={styles.text}>{getFutureFormattedDate(lastInvoiceDate?.get(), 1, "year", "DD MMM YYYY")}</div>
               </div>
             </div>
           </div>
@@ -36,29 +65,16 @@ const ModalSubscribe3 = ({ className = "", onClose }) => {
             <div className={styles.label}>Last invoice</div>
             <div className={styles.frameParent}>
               <div className={styles.inputFieldBaseParent}>
-                <div className={styles.text2}>{`26 February 2025 `}</div>
-                <div className={styles.text3}>USD 12</div>
+                <div className={styles.text2}>{getFormattedDate(lastInvoiceDate?.get(), "DD MMM YYYY")}</div>
+                <div className={styles.text3}>USD 12.00</div>
               </div>
-              <div className={styles.text4}>Card ending with 2568</div>
+              <div className={styles.text4}>Card ending with {cardEnding?.get()}</div>
             </div>
           </div>
         </div>
       </div>
-      <div className={styles.modalActions}>
-        <Button className={styles.buttonBase} type="default">
-          Cancel
-        </Button>
-        <Button className={styles.buttonBase} type="primary">
-          Next
-        </Button>
-      </div>
-    </div>
+    </Modal>
   );
-};
-
-ModalSubscribe3.propTypes = {
-  className: PropTypes.string,
-  onClose: PropTypes.func,
 };
 
 export default ModalSubscribe3;
