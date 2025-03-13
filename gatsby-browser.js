b7f407b208daa4556fd0574c7fd80da4044fd593
waleedshkt <waleedshkt@gmail.com>
2025-03-14 01:00:30 +0500

    Set up root page element

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,32 @@
+import "./styles/global.css";
+    
+import React from "react";
+import { Script } from "gatsby";
+import Loader from "./src/components/Loader";
+import Message from "./src/components/Message";
+import { ConfigProvider } from "antd";
+import { AuthProvider } from "./src/helpers/auth/firebase/user";
+import useAuth from "./src/hooks/useAuth";
 
-    import "./styles/global.css";
-    
-    
+const withAuthProvider = Component => props => (
+    <AuthProvider>
+        <Component {...props} />
+    </AuthProvider>
+);
+
+export const wrapRootElement = withAuthProvider(({ element }) => {
+    const initialLoad = useAuth();
+
+    return (
+        <>
+            <ConfigProvider 
+                theme={{}}
+            >
+                {element}
+                <Loader />
+                <Message />
+            </ConfigProvider>    
+            <Script src="https://www.2checkout.com/checkout/api/2co.min.js" strategy="idle" />
+        </>
+    );
+});
