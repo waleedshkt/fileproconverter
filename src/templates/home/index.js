1e32bb605c7844c6e436d1b7e7bcc07d30834cea
waleedshkt <waleedshkt@gmail.com>
2025-03-24 16:29:05 +0500

    Completed page logic and placed in tempalte directory

--- src/templates/home/index.js
+++ src/templates/home/index.js
@@ -1,26 +1,29 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useEffect } from "react";
 import { useHookstate as useState } from "@hookstate/core";
 import { navigate } from "gatsby";
 import { Button, Divider, Upload } from "antd";
 import { FileOutlined } from "@ant-design/icons";
-import Header from "../components/Header";
-import USPSection from "../components/UspSection";
-import AddMoreSection from "../components/AddMoreFilesSection";
-import HowToConvert from "../components/HowToConvertSection";
-import Conversions from "../components/Conversions";
-import FormatsOverview from "../components/FormatsOverview";
-import Footer from "../components/Footer";
+import SEO from "../../components/SEO";
+import Header from "../../components/Header";
+import USPSection from "../../components/UspSection";
+import AddMoreSection from "../../components/AddMoreFilesSection";
+import HowToConvert from "../../components/HowToConvertSection";
+import Conversions from "../../components/Conversions";
+import FormatsOverview from "../../components/FormatsOverview";
+import Footer from "../../components/Footer";
 import * as styles from "./index.module.css";
 
-import HeroInputDot from "../images/svg/dot.svg";
-import HeroInputChevronDown from "../images/svg/chevronDown.svg";
+import HeroInputDot from "../../images/svg/dot.svg";
+import HeroInputChevronDown from "../../images/svg/chevronDown.svg";
 
-import { selection, misc } from "../globalStates";
+import { selection, misc } from "../../globalStates";
 
-import lazyLoad from "../helpers/lazyLoad";
+import lazyLoad from "../../helpers/lazyLoad";
 const FormatsModal = lazyLoad("Modals/Formats");
 
-const Page = ({ data }) => {
+const Page = ({ pageContext }) => {
+  const { data } = pageContext;
+
   const selection_ = useState(selection); //global
   const misc_ = useState(misc); //global
   const beginUpload = useState(false);
@@ -36,7 +39,7 @@ const Page = ({ data }) => {
         from = format;
         to = p.to;
       }else {
-        from = p.format;
+        from = p.from;
         to = format;
       }
 
@@ -46,22 +49,29 @@ const Page = ({ data }) => {
     from = from ? from.substr(0).replaceAll(".", "") : from;
     to = to ? to.substr(0).replaceAll(".", "") : to;
 
+    console.log(from, to);
+
     if(from && to) {
-      navigate(`/convert/${from.toLowerCase()}-to-${to.toLowerCase()}`);
-    }else {
-      navigate(`/${from?.toLowerCase() || to?.toLowerCase()}-converter`);
+      navigate(`/${from.toLowerCase()}-to-${to.toLowerCase()}`);
+    }else if(from && !Boolean(to)) {
+      navigate(`/${from?.toLowerCase()}-converter`);
     }
-  }, []);
+
+    toggleFormatsModal(false);
+  }, [data.type]);
 
   const beforeUpload = useCallback((file) => {
       let fileFormat = file.name?.split(".");
       fileFormat.shift();
-      fileFormat = fileFormat.join(".").toUpperCase(); // for cases such as .TAR.GZ 
+      fileFormat = fileFormat.join(".").toLowerCase(); // for cases such as .TAR.GZ 
+      fileFormat = "." + fileFormat;
 
-      if(!formats.includes(fileFormat)) { // here formats array prop comes from top 'data' page prop
+      console.log(data.formatExtensions, fileFormat);
+
+      if(!data.formatExtensions.includes(fileFormat)) { // here formats array prop comes from top 'data' page prop
         
         // not yet supported format
-        content = formats.length > 1 ? "Sorry. This format is not yet supported." : "Sorry. The file format is invalid."
+        let content = (data.type !== "both" && data.formatExtensions.length > 1) ? "Sorry. This format is not yet supported." : "Sorry. The file format is invalid."
 
         misc_.message.set({ type: "error", content });
         return false;
@@ -71,18 +81,31 @@ const Page = ({ data }) => {
       let fileSize = file.size / 1024 / 1024 / 1024;
       if(fileSize > 1) { // GB
         // above size limit
-        messageApi.open({ type: "error", content: "1GB size limit exceeded" });
+        misc_.message.set({ type: "error", content: "1GB size limit exceeded" });
         return false;
       }
 
+      let newFrom = true,
+          from_;
+
       selection_.set(p => {
-        return { ...p, files: p.files.concat([file]), from: fileFormat };
+
+        newFrom = true;
+        if(!!data?.fromTo[0]) {
+          newFrom = false;
+          from_ = p.from;
+        }else {
+          from_ = fileFormat;
+        }
+
+        return { files: p.files.concat([file]), from: from_ , to: newFrom ? "" : p.to };
       });
 
-      selection_.files.merge([file]);
+      newFrom && navigate(`/${from_}-converter`);
+
       return false;
 
-  }, []);
+  }, [data?.formatExtensions, data?.fromTo]);
 
   const handleUpload = useCallback(() => beginUpload?.set(true), []);
 
@@ -95,6 +118,12 @@ const Page = ({ data }) => {
     });
   }, []);
 
+  useEffect(() => {
+    selection_.set(p => {
+      return {...p, from: data?.fromTo[0] || "", to: data?.fromTo[1] || ""};
+    });
+  }, [data?.fromTo]);
+
   return (
     <>
       <div className={styles.page}>
@@ -103,14 +132,15 @@ const Page = ({ data }) => {
           <div className={styles.main}>
             <div className={styles.adWrapper}>
               <div className={styles.ad} />
+              <div className={styles.ad_} />
             </div>
             <section className={styles.content}>
               <div className={styles.content1}>
                 <div className={styles.heroSection}>
                   <div className={styles.fileConverterParent}>
-                    <div className={styles.fileConverter}>File Converter</div>
+                    <div className={styles.fileConverter}>{data.type === "main" ? "File Converter" : (data.type === "both" ? `${data.seo.title} Converter` : data.seo.title)}</div>
                     <div className={styles.securelyConvertFiles}>
-                      Securely convert files online from one format to another
+                      {data.type === "main" ? "Securely convert files online from one format to another" : "Securely convert online from one format to another"}
                     </div>
                   </div>
                   <div className={styles.inputParent}>
@@ -131,6 +161,8 @@ const Page = ({ data }) => {
                   </div>
                 </div>
                 <div className={styles.ad1} />
+                <div className={styles.ad1_} />
+                <div className={styles.ad1__} />
                 {selection_?.files?.length ? (
                   <AddMoreSection 
                     from={selection_?.from?.get()}
@@ -160,25 +192,30 @@ const Page = ({ data }) => {
                   </div>
                 )}
                 <div className={styles.ad2} />
+                <div className={styles.ad2_} />
+                <div className={styles.ad2__} />
                 <USPSection />
               </div>
               <HowToConvert 
                 from={selection_.from?.get()} 
                 to={selection_.to?.get()} 
               />
-              <Conversions 
-                heading={""}
-                linksArr={[]}
-              />
+              {data?.conversions?.map((o, i) => (
+                <Conversions 
+                  key={i}
+                  heading={o.heading}
+                  linksArr={o.linksArray}
+                />
+              ))}
               <Divider className={styles.divider} />
-              <FormatsOverview 
-                fromToArr={[selection_?.from?.get(), selection_?.to?.get()]}
-                info={{}}
-              />
+              {/* <FormatsOverview
+                info={data?.formatOverview}
+              /> */}
               <div className={styles.ad3} />
             </section>
             <div className={styles.adWrapper}>
               <div className={styles.ad4} />
+              <div className={styles.ad4_} />
             </div>
           </div>
         </main>
@@ -199,3 +236,14 @@ const Page = ({ data }) => {
 };
 
 export default Page;
+
+export const Head  = ({ location: { pathname }, pageContext }) => {
+  const { data: { seo } } = pageContext;
+
+  return (
+    <SEO 
+      {...seo}
+      pathname={pathname}
+    />
+  );
+};
