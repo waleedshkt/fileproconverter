38802cc869f6e2a83a8945e7c8b995d88198d404
waleedshkt <waleedshkt@gmail.com>
2025-03-24 16:21:46 +0500

    Create pages with final-info json

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,26 @@
+const path = require("path");
+const pageData = require("./src/data/final-info.json");
+
 exports.onCreateWebpackConfig = ({ getConfig, actions }) => {
     if(getConfig().mode === 'production') {
         actions.setWebpackConfig({
             devtool: false
         });
     }
+};
+
+exports.createPages = async ({ graphql, actions, reporter }) => {
+    const { createPage } = actions;
+
+    const pageTemplate = path.resolve("src/templates/home/index.js");
+
+    pageData.forEach((info, i) => {
+        createPage({
+            path: info.path,
+            component: pageTemplate,
+            context: {
+                data: info
+            }
+        });
+    });
 };
